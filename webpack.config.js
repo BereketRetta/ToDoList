cd616f2965752542705c46ef9f229f3b497fc738
Bereket Retta <bereket@etmsoftwareplc.com>
2022-01-18 17:34:55 +0300

    some linter fixes

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+import HtmlWebpackPlugin from  'html-webpack-plugin';
 
 module.exports = {
   mode: 'development',
